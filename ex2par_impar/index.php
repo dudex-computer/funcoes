53c3eb651f1af2d9a5722d36aa2212def3fe1f6f
dudex-computer <duda.mefalmeida@gmail.com>
2025-04-29 11:32:21 -0300

    Maria Eduarda e Lorena Cani

--- ex2par_impar/index.php
+++ ex2par_impar/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Funções</title>
+    <title>Numero aleatório</title>
 </head>
 <body>
     <fieldset>
@@ -11,7 +11,7 @@
         <form action="calcular.php" method="post">
         Valor 1: <input type="number" name="valor1" required><br>
         Valor 2: <input type="number" name="valor2" required><br>
-        <input type="submit" value="Resultado">
+        <input type="submit" value="Calcular Média">
         </form>
     </fieldset>
 </body>
