ceb40e9f7f1fc088f10f2286d27e5b765157f77e
dudex-computer <duda.mefalmeida@gmail.com>
2025-04-29 11:34:40 -0300

    Maria Eduarda e Lorena Cani

--- ex2par_impar/index.php
+++ ex2par_impar/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Digite o número</title>
+    <title>Funções</title>
 </head>
 <body>
     <fieldset>
